6bad88773cc4e621105c8509ae088e1d0ceabbb6
lani1n <kiang45@outlook.com>
2024-09-06 21:55:46 +0700

    update

--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-cwd = os.getcwd()
-
-print("\n--> {} <--\n".format(cwd))
--- utility/image_compresser.py
+++ utility/image_compresser.py
@@ -5,10 +5,10 @@ cwd = os.getcwd()
 quality = 75
 
 for filename in os.listdir(cwd):
-    if filename.endswith('.jpg') or filename.endswith('.png'):
+    if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
         img = Image.open(filename)
         width, height = img.size
         new_size = (int(width*1), int(height*1))
         img = img.resize(new_size)
-        compressed_filename = 'new_' + filename
+        compressed_filename = "new_" + filename
         img.save(compressed_filename, quality=quality)
